579c6d623149af9f1aadf2222c8f8668955b4e5a
Neeya <neeya.201718@ncit.edu.np>
2022-08-04 12:47:50 +0545

    First Commit

---
+++ js/index.js
@@ -0,0 +1,9 @@
+function text_me(){
+    var form1 = document.getElementById('form1');
+    if(form1.style.visibility=="hidden"){
+        form1.style.visibility="visible";
+    }
+    else{
+        form1.style.visibility="hidden";
+    }
+}
